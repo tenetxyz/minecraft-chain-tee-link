4b791714b6f639d8030e8c5a9499685e73b4fb1c
Curtis Chong <curtischong5@gmail.com>
2023-05-10 17:49:37 -0400

    feat: removed old simulator client

--- packages/old-simulator-client/src/components/PositionComponent.ts
+++
@@ -1,9 +0,0 @@
-import { World } from "@latticexyz/recs";
-import { defineVoxelCoordComponent } from "@latticexyz/std-client";
-
-export function definePositionComponent(world: World) {
-  return defineVoxelCoordComponent(world, {
-    id: "Position",
-    metadata: { contractId: "component.Position" },
-  });
-}
