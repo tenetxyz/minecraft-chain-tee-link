bd1a1719d9b3931c7df67b205bd74e42c354f02e
Curtis Chong <curtischong5@gmail.com>
2023-05-10 17:34:42 -0400

    feat: make simulator-client a good proxy

--- packages/simulator-client/vite.config.ts
+++ packages/simulator-client/vite.config.ts
@@ -1,41 +1,29 @@
 import { defineConfig } from "vite";
-import path from "path";
-import glob from "glob";
-
-const config = {
-  // server: {
-  //   port: 4000,
-  //   fs: {
-  //     strict: false,
-  //   },
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     target: "es2020",
-  //   },
-  //   exclude: ["@latticexyz/network"],
-  //   include: [
-  //     "proxy-deep",
-  //     "ethers/lib/utils",
-  //     "bn.js",
-  //     "js-sha3",
-  //     "hash.js",
-  //     "bech32",
-  //     "long",
-  //     "protobufjs/minimal",
-  //     "debug",
-  //     "is-observable",
-  //     "nice-grpc-web",
-  //     "@improbable-eng/grpc-web",
-  //   ],
-  // },
-  root: "src",
-  build: {
-    outDir: "./dist",
-    rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, "src", "*.ts")),
+export default defineConfig({
+  server: {
+    port: 3000,
+    fs: {
+      strict: false,
     },
   },
-};
-console.log(config);
-export default config;
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+    exclude: ["@latticexyz/network"],
+    include: [
+      "proxy-deep",
+      "ethers/lib/utils",
+      "bn.js",
+      "js-sha3",
+      "hash.js",
+      "bech32",
+      "long",
+      "protobufjs/minimal",
+      "debug",
+      "is-observable",
+      "nice-grpc-web",
+      "@improbable-eng/grpc-web",
+    ],
+  },
+});
