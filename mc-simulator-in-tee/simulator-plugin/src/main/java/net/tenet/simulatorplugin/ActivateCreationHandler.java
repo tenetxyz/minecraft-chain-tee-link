a9c4e70542366b1154c968da72cb9d2d6b44e39a
Curtis Chong <curtischong5@gmail.com>
2023-05-11 12:25:01 -0400

    added code to find the uppernortheast and lower southwest corners

--- mc-simulator-in-tee/simulator-plugin/src/main/java/net/tenet/simulatorplugin/ActivateCreationHandler.java
+++ mc-simulator-in-tee/simulator-plugin/src/main/java/net/tenet/simulatorplugin/ActivateCreationHandler.java
@@ -2,13 +2,16 @@ package net.tenet.simulatorplugin;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
 import java.io.*;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
+
 
 class ActivateCreationHandler implements HttpHandler {
     Plugin pluginReference; // This reference helps us modify the world on the main thread via a bukkit scheduler
@@ -17,28 +20,61 @@ class ActivateCreationHandler implements HttpHandler {
         this.pluginReference = pluginReference;
     }
 
-    @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String requestBody = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
-                .lines().collect(Collectors.joining("\n"));
-        Gson gson = new Gson();
-        ActivateCreationRequest activateCreationRequest = gson.fromJson(requestBody, ActivateCreationRequest.class);
+        Pair<Integer, String> responsePair = tryActivateCreation(exchange);
+        int statusCode = responsePair.getLeft();
+        String response = responsePair.getRight();
 
-        String response;
-        int statusCode;
-        try {
-            setCreation(activateCreationRequest);
-            response = "Creation activated!";
-            statusCode = 200;
-        } catch (Exception e) {
-            response = "Could not activate creation. " + e.getMessage();
-            statusCode = 400;
-        }
         System.out.println(response);
         exchange.sendResponseHeaders(statusCode, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    // return shte statusCode and the response
+    private Pair<Integer, String> tryActivateCreation(HttpExchange exchange){
+        ActivateCreationRequest activateCreationRequest;
+        try{
+            String requestBody = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
+                    .lines().collect(Collectors.joining("\n"));
+            Gson gson = new Gson();
+            activateCreationRequest = gson.fromJson(requestBody, ActivateCreationRequest.class);
+        }catch(Exception e){
+            return Pair.of(400, "Could not parse request body: " + e.getMessage());
+        }
+
+        if(activateCreationRequest.blocks.length == 0){
+            return Pair.of(400, "No blocks provided. Creations must have at least one block.");
+        }
+
+        try {
+            setCreation(activateCreationRequest);
+
+            Block upperNorthEastBlock = calculateCornerBlock(activateCreationRequest.blocks, (Block block, Block resultBlock) ->
+                    block.x >= resultBlock.x &&
+                            block.y >= resultBlock.y &&
+                            block.z >= resultBlock.z);
+            Block lowerSouthwestBlock = calculateCornerBlock(activateCreationRequest.blocks, (Block block, Block resultBlock) ->
+                    block.x <= resultBlock.x &&
+                            block.y <= resultBlock.y &&
+                            block.z <= resultBlock.z);
+            return Pair.of(200, "Creation activated!");
+        } catch (Exception e) {
+            return Pair.of(400, "Could not activate creation: " + e.getMessage());
+        }
+    }
+
+    private static Block calculateCornerBlock(Block[] blocks, BiPredicate<Block, Block> evaluationFunction) {
+        Block resultBlock = blocks[0];
+
+        for (Block block : blocks) {
+            if (evaluationFunction.test(block, resultBlock)) {
+                resultBlock = block;
+            }
+        }
+
+        return resultBlock;
     }
 
     private void setCreation(ActivateCreationRequest activateCreationRequest ) throws IllegalArgumentException{
