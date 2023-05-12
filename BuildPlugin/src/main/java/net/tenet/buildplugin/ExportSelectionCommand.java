646486e8f8ec6af1bd4e3c5a3c83fccaca86ce2e
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-05-12 15:20:19 -0600

    Add block material

--- BuildPlugin/src/main/java/net/tenet/buildplugin/ExportSelectionCommand.java
+++ BuildPlugin/src/main/java/net/tenet/buildplugin/ExportSelectionCommand.java
@@ -1,5 +1,6 @@
 package net.tenet.buildplugin;
 
+import com.google.gson.*;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -37,10 +38,27 @@ public class ExportSelectionCommand implements CommandExecutor {
             if(worldeditSelectionFile.exists()) {
                 try {
                     String data = new String(Files.readAllBytes(worldeditSelectionFile.toPath()));
+                    JsonElement jsonElement = JsonParser.parseString(data);
+                    JsonArray selectionCoords = jsonElement.getAsJsonArray();
+
+                    Block[] blocks = new Block[selectionCoords.size()];
+                    for(int i = 0; i < selectionCoords.size(); i++){
+                        JsonElement jsonElement1 = selectionCoords.get(i);
+                        JsonObject coordData = jsonElement1.getAsJsonObject();
+                        int x = coordData.get("x").getAsInt();
+                        int y = coordData.get("y").getAsInt();
+                        int z = coordData.get("z").getAsInt();
+                        // get block at this location
+                        org.bukkit.block.Block block = player.getWorld().getBlockAt(x, y, z);
+                        blocks[i] = new Block(new Location(x, y, z), block.getType());
+                    }
+                    Creation creation = new Creation(blocks);
+                    Gson gson = new Gson();
+                    String creationJson = gson.toJson(creation);
 
                     TextComponent message = new TextComponent("Click here to copy the exported JSON of your selection!");
                     message.setColor(ChatColor.BLUE);
-                    message.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, data));
+                    message.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, creationJson));
 
                     player.spigot().sendMessage(message);
                 } catch (IOException e) {
