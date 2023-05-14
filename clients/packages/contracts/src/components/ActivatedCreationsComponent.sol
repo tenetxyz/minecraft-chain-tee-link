4021fe12abe2de278364215e915e5b668fc59e30
Curtis Chong <curtischong5@gmail.com>
2023-05-14 00:51:37 -0400

    fixed bug where components where we're saving arrays of uint256 crash

--- clients/packages/contracts/src/components/ActivatedCreationsComponent.sol
+++ clients/packages/contracts/src/components/ActivatedCreationsComponent.sol
@@ -10,7 +10,8 @@ contract ActivatedCreationsComponent is Uint256ArrayBareComponent {
     constructor(address world) Uint256ArrayBareComponent(world, ID) {}
 
     function addCreation(uint256 ownerEntityId, uint256 creationEntityId) public virtual {
-        uint256[] memory oldCreations = getValue(ownerEntityId);
+        bytes memory oldCreationsBytes = getRawValue(ownerEntityId);
+        uint256[] memory oldCreations = oldCreationsBytes.length == 0 ? new uint256[](0) : abi.decode(oldCreationsBytes, (uint256[]));
         uint256[] memory newCreations = new uint256[](oldCreations.length + 1);
         for (uint256 i = 0; i < oldCreations.length; i++) {
             newCreations[i] = oldCreations[i];
