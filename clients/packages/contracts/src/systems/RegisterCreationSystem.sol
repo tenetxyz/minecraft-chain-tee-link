3e016551dab28b7baa69a8840b6cc70596c29bf5
Curtis Chong <curtischong5@gmail.com>
2023-05-14 22:03:29 -0400

    using Strings utils library to convert uinst256 to ascii

--- clients/packages/contracts/src/systems/RegisterCreationSystem.sol
+++ clients/packages/contracts/src/systems/RegisterCreationSystem.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 import "solecs/System.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { getAddressById, addressToEntity } from "solecs/utils.sol";
 import { BlocksComponent, ID as BlocksComponentID} from "../components/BlocksComponent.sol";
@@ -30,7 +31,10 @@ contract RegisterCreationSystem is System {
 
         // check that this creation hasn't been made before
         uint256 creationEntityId = getCreationHash(opcBlocks);
-        require(!blocksComponent.has(creationEntityId), string(abi.encodePacked("This creation has already been created. The entity with the same blocks is ", "This creation's entityId is " , creationEntityId)));
+//        require(!blocksComponent.has(creationEntityId), string(abi.encode("This creation has already been created. This creation's entityId is " , creationEntityId)));
+        require(!blocksComponent.has(creationEntityId), string.concat("This creation has already been created. This creation's entityId is ", Strings.toString(creationEntityId)));
+//        require(!blocksComponent.has(creationEntityId), string("This creation has already been created. This creation's entityId is " , string(creationEntityId)));
+//        require(!blocksComponent.has(creationEntityId), "This creation has already been created");
 
         // now we can safely make this new creation
         OpcBlock[] memory repositionedOpcBlocks = repositionBlocksSoLowerSouthwestCornerIsOnOrigin(opcBlocks);
